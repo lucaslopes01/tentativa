b38af2d7d53cc99130bab433df154e7c3ddaf41a
lucaslopes01 <lucaslsantos159@gmail.com>
2023-03-04 20:03:45 -0300

    frontend

---
+++ stocks.js/stocks.js
@@ -0,0 +1,16 @@
+class Item{
+    constructor(){
+        this.id = 0;
+        this.estoque = '';
+        this.nomeItem = '';
+        this.valor = 0
+
+    }
+    adicionar(){
+        alert('vamos adicionar um item!');
+    }
+    excluir(){
+        alert('item deletado!')
+    }
+}
+var item = new Item();
